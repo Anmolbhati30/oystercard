93e92997ed11bcec7024b897b1d774e44e0176ce
parsa <parsamesg@gmail.com>
2021-09-29 14:35:12 +0100

    add unit test to charge a fare, otherwise a penalty

--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -1,4 +1,5 @@
 require "journey"
+require "station"
 
 describe Journey do
   let!(:kings_cross) { instance_double(Station, :zone => 1, name: "Kings Cross") }
@@ -21,6 +22,30 @@ describe Journey do
       subject.exit_at(victoria)
       expected_journey = { entry: kings_cross, exit: victoria }
       expect(subject.create_record).to eq expected_journey
+    end
+  end
+
+  describe "#fare" do
+    context "if no entry station" do
+      it "returns penalty charge" do
+        expect(subject.fare).to eq described_class::PENALTY_CHARGE
+      end
+    end
+    context "if entry station" do
+      context "and no exit station" do
+        it "returns penalty charge" do
+          subject.enter_at(kings_cross)
+          expect(subject.fare).to eq described_class::PENALTY_CHARGE
+        end
+      end
+
+      context "and exit station" do
+        it "returns minimum fare" do
+          subject.enter_at(kings_cross)
+          subject.exit_at(victoria)
+          expect(subject.fare).to eq described_class::MIN_CHARGE
+        end
+      end
     end
   end
 end
