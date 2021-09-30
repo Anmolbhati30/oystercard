0b0562b7dcb6090d3249903a1dd6fe46211f1492
Anmol <bhatianmol566@yahoo.co.uk>
2021-09-30 16:38:23 +0100

    Add JourneyLog class with some tests

--- lib/journeylog.rb
+++ lib/journeylog.rb
@@ -1,7 +1,28 @@
 class JourneyLog
-  attr_reader :journey_class
+  attr_reader :current_journey
+
   def initialize(journey_class = Journey)
     @journey_class = journey_class
+    @current_journey
+    @journeys = []
+  end
 
+  def start(entry_station)
+    @current_journey = @journey_class.new(entry_station)
+  end
+
+  def finish(exit_station)
+    @journeys.push(@current_journey.finish(exit_station))
+    return @journeys.last.fare
+  end
+
+  def journeys
+    @journeys.dup
+  end
+
+  private
+
+  def current_journey
+    @current_journey ||= @journey_class.new
   end
 end
--- spec/journeylog_spec.rb
+++ spec/journeylog_spec.rb
@@ -1,9 +1,39 @@
 require "journeylog"
+require "journey"
 
 describe JourneyLog do
+  let!(:kings_cross) { instance_double(Station, :name => :kings_cross) }
+  let!(:victoria) { instance_double(Station, :name => :victoria) }
   describe "#initialize" do
     it "initializes with a journey class parameter" do
-      expect(subject.journey_class.new.class).to eq Journey
+      expect(subject.instance_variable_get(:@journey_class).new).to be_a Journey
+    end
+
+    it "has no journeys to begin with" do
+      expect(subject.journeys).to be_empty
+    end
+  end
+
+  describe "#start" do
+    it "starts a new journey with a given entry station" do
+      subject.start(kings_cross)
+      new_journey = subject.instance_variable_get(:@current_journey)
+      expect(new_journey.entry_station).to eq kings_cross
+    end
+  end
+
+  describe "#finish" do
+    it "records the finished journey" do
+      subject.start(kings_cross)
+      subject.finish(victoria)
+      finished_journey = subject.(:@current_journey).finish(victoria)
+      expect(subject.journeys).to include finished_journey
+    end
+
+    it "returns the journeys fare" do
+      subject.start(kings_cross)
+      finished_journey = subject.instance_variable_get(:@current_journey).finish(victoria)
+      expect(subject.finish(victoria)).to eq finished_journey.fare
     end
   end
 end
